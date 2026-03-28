08fbfaf32faf22d09ce880acd0618a2c28c666ab
Haura <haurafahimahazzahras@gmail.com>
2026-03-28 10:04:37 +0700

    pertemuan 4: perulangan

---
+++ pertemuan4/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+a: cout<<"hello world"<<endl;
+goto d;
+b: cout<<"fasilkom ti"<<endl;
+c: cout<<"ilmu komputer"<<endl;
+goto b; 
+d: cout<<"iklc"<<endl;
+goto c;
---
+++ pertemuan4/while.cpp
@@ -0,0 +1,23 @@
+//akan terus berulang selama kondisinya true 
+//dan berhenti jika salah
+#include<iostream>
+using namespace std;
+int  main(){
+
+    //(WHILE)
+    // int i = 1; 
+    // while (i<= 10) {
+    //     if(i%2 == 0){
+    //         cout<<i<<" ";} i++;
+    // }
+
+    //(DO WHILE)
+    // int i = 1; 
+    // do { cout<<i<<" ";} while(i<=0)
+    //jadi bedanya kalo do while dia walaupun salah diawal.
+    //tetep dilakuin meski salah, tp gadilahuin kalo salah hanya untuk perulangan selanjutnya
+    // kalo yang do while kondisinya salah dia bakal nampilin terus menerus?
+
+    
+
+}
